016f950b62bc2c656fbef00629a94a8228c62c49
parkvsmin <80439212+parkvsmin@users.noreply.github.com>
2022-07-13 22:33:56 +0900

    Update StudentView.java

--- src/com/iu/object1/ex1/StudentView.java
+++ src/com/iu/object1/ex1/StudentView.java
@@ -2,10 +2,33 @@ package com.iu.object1.ex1;
 
 public class StudentView {
 	
-	public void ViewAll() {
+	//viewMessage
+	//문자열을 받아서 그 문자열을 출력
+	public void view(String message) {
+		System.out.println(message);
+	}
+	
+	//viewOne
+	//학생 한명의 정보를 받아서 모든 정보를 출력
+	public void view(Student student) {
+		System.out.println(student.name);
+		System.out.println(student.num);
+		System.out.println(student.total);
+		System.out.println(student.avg);
+	}
+	
+	//viewAll
+	//학생들의 정보를 받아서 모든 정보를 출력
+	public void view(Student [] students) {
+		for(int i=0;i<students.length;i++) {
+			System.out.println(students[i].name);
+			System.out.println(students[i].num);
+			System.out.println(students[i].total);
+			System.out.println(students[i].avg);
+		}
+		
 		
 	}
-	//ViewAll
-	//학생들의 정보를 받아서 모든 정보를 출력
+	
 
 }
