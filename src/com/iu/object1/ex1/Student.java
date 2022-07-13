7c1087f2278fce54812d7a3e5c195c19dc91210c
parkvsmin <sdfjkl658@naver.com>
2022-07-13 22:41:35 +0900

    0713

--- src/com/iu/object1/ex1/Student.java
+++ src/com/iu/object1/ex1/Student.java
@@ -3,6 +3,7 @@ package com.iu.object1.ex1;
 public class Student {
 
 	String name;
+	int num;
 	int nums;
 	int kor;
 	int eng;
---
+++ src/com/iu/object3/Count.java
@@ -0,0 +1,19 @@
+package com.iu.object3;
+
+public class Count {
+	
+	
+	public void hap(int num1, int num2) {
+		System.out.println(num1+num2);
+	}
+	
+	public void hap(int num1, double num2) {
+		System.out.println(num1*num2);
+	}
+	
+	public void hap(int num1, int num2, int num3) {
+		
+	}
+
+	public int hap(int num2, int num1) {}
+}
